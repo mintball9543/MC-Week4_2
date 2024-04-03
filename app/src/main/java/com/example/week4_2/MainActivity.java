d36d60df76c088fb2bd3c0b235117a6c7b63c75c
mintball9543 <minseokcomputer@gmail.com>
2024-04-04 02:08:32 +0900

    Week4 Lecture practice

---
+++ app/src/main/java/com/example/week4_2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.week4_2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //setContentView(R.layout.activity_main);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        baseLayout.setBackgroundColor(Color.rgb(0,255,0));
+        setContentView(baseLayout,params);
+
+        Button btn = new Button(this);
+        btn.setText("버튼입니다.");
+        btn.setBackgroundColor(Color.MAGENTA);
+        baseLayout.addView(btn);
+        btn.getLayoutParams().width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        btn.getLayoutParams().height = 100;
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                Toast.makeText(getApplicationContext(),
+                        "코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
